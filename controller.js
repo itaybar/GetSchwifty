b8308175df236a927d9d0c793cbb0247be837425
itaybar <46649219+itaybar@users.noreply.github.com>
2022-07-03 17:40:39 +0300

    Added main with functions signature

--- controller.js
+++ controller.js
@@ -1,3 +1,12 @@
 import {generateTable} from './model.js';
 
-console.log(generateTable(3))
+    let numberOpposites = 0;
+
+    let size = 3;
+    do {
+        var table = generateTable(size);
+    } while (!checkTableValidity(table));
+    console.log(table);
+}
+
+main();
